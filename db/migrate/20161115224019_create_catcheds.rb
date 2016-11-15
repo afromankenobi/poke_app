1040e8915b34fa5ab18767c7485851c0f03dedd8
Jorge Vargas Silva <jv.vargass@gmail.com>
2016-11-15 19:44:54 -0300

    catched and bootstrap cdn

---
+++ db/migrate/20161115224019_create_catcheds.rb
@@ -0,0 +1,11 @@
+class CreateCatcheds < ActiveRecord::Migration[5.0]
+  def change
+    create_table :catcheds do |t|
+      t.references :user, foreign_key: true
+      t.references :pokemon, foreign_key: true
+      t.integer :level
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115223846) do
+ActiveRecord::Schema.define(version: 20161115224019) do
+
+  create_table "catcheds", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "pokemon_id"
+    t.integer  "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pokemon_id"], name: "index_catcheds_on_pokemon_id"
+    t.index ["user_id"], name: "index_catcheds_on_user_id"
+  end
 
   create_table "pokemons", force: :cascade do |t|
     t.string   "name"
