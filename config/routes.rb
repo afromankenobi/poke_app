fca7fe13b9109a7b85251d40e95852063320d6c0
Jorge Vargas Silva <jv.vargass@gmail.com>
2016-11-18 21:11:37 -0300

    routes for all

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
+
   resources :pokemons
+
+  resources :catcheds, only: [:index, :new, :create] do
+    member do
+      get :level_up
+    end
+  end
+
+  root 'pokemons#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
