8add603863f84000fac3fa44b19b1559f042b9aa
Jorge Vargas Silva <jv.vargass@gmail.com>
2016-11-18 21:10:29 -0300

    set default level to catched

--- app/models/catched.rb
+++ app/models/catched.rb
@@ -1,4 +1,12 @@
 class Catched < ApplicationRecord
   belongs_to :user
   belongs_to :pokemon
+
+  before_save :set_default_level
+
+  private
+
+  def set_default_level
+    self.level = 0 unless self.level
+  end
 end
